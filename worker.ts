fc9d10651e7865fa2e2b1a47fda9ef3c27287733
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-18 03:06:34 -0500

    Setup asset binding

--- worker.ts
+++ worker.ts
@@ -6,7 +6,10 @@ import schema from "db:schema";
 export default bootstrap(
 	{ orm: { schema }, rateLimit: { limit: 1000, period: 60 } },
 	{
-		async onRequest() {
+		async onRequest(_, { ASSETS }) {
+			let assetResponse = await ASSETS.fetch(request());
+			if (assetResponse.ok) return assetResponse;
+
 			let result = await orm().query.users.findMany();
 			let url = new URL(request().url);
 			return new Response(
@@ -19,7 +22,10 @@ export default bootstrap(
 
 declare module "@edgefirst-dev/core" {
 	export interface Bindings {
+		ASSETS: Fetcher;
 		DB: D1Database;
+		FS: R2Bucket;
+		KV: KVNamespace;
 	}
 
 	type Schema = typeof schema;
