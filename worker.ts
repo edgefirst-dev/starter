0aed56f0abc566540b09e4a80c8c53ce36257aef
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-18 02:00:48 -0500

    Configure bindings, schema and run simple query on worker

--- worker.ts
+++ worker.ts
@@ -1,19 +1,27 @@
-import { request } from "@edgefirst-dev/core";
+import { orm, request } from "@edgefirst-dev/core";
 import { bootstrap } from "@edgefirst-dev/core/worker";
 
+import schema from "db:schema";
+
 export default bootstrap(
-  { rateLimit: { limit: 1000, period: 60 } },
+  { orm: { schema }, rateLimit: { limit: 1000, period: 60 } },
   {
     async onRequest() {
+      let result = await orm().query.users.findMany();
       let url = new URL(request().url);
-      return new Response(`Hello from ${url.pathname}`, {
-        status: 200,
-        statusText: "OK",
-      });
+      return new Response(
+        `Hello from ${url.pathname}; Users "${result.length}"`,
+        { status: 200, statusText: "OK" }
+      );
     },
   }
 );
 
 declare module "@edgefirst-dev/core" {
-  export interface Bindings {}
+  export interface Bindings {
+    DB: D1Database;
+  }
+
+  type Schema = typeof schema;
+  export interface DatabaseSchema extends Schema {}
 }
