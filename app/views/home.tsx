4e9a07901763346927ab4e25a725534e263e8203
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-20 12:58:38 -0500

    Add link to profile on / if the user is authenticated

--- app/views/home.tsx
+++ app/views/home.tsx
@@ -1,16 +1,32 @@
+import { ok } from "app:helpers/response";
+import { querySession } from "app:helpers/session";
 import type * as Route from "types:views/+types.home";
 import { Link } from "react-router";
 
-export default function Home(props: Route.ComponentProps) {
+export async function loader({ request }: Route.LoaderArgs) {
+	let session = await querySession(request);
+	if (session) return ok({ isSignedIn: true });
+	return ok({ isSignedIn: false });
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
 	return (
 		<main className="flex justify-center items-center flex-col min-h-dvh gap-8">
 			<aside className="flex gap-2 absolute top-0 right-0 pt-4 pr-4">
-				<Link to="/login" className="hover:underline">
-					Login
-				</Link>
-				<Link to="/register" className="hover:underline">
-					Register
-				</Link>
+				{loaderData.isSignedIn ? (
+					<Link to="/profile" className="hover:underline">
+						Profile
+					</Link>
+				) : (
+					<>
+						<Link to="/login" className="hover:underline">
+							Login
+						</Link>
+						<Link to="/register" className="hover:underline">
+							Register
+						</Link>
+					</>
+				)}
 			</aside>
 
 			<header className="flex flex-col gap-4 text-center">
