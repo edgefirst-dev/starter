6ef6a4e893ee6f66bbfe25a0fc99ea24becfeb78
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-20 05:15:20 -0500

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,16 @@
-import { index, route } from "@react-router/dev/routes";
+import { index, layout, route } from "@react-router/dev/routes";
 
 export const routes = [
 	index("./views/home.tsx"),
+	route("/profile", "./views/profile.tsx"),
+
+	// Auth Routes
+	layout("./layouts/auth.tsx", [route("register", "./views/register.tsx")]),
+
+	// API Routes
+	route("/api/files/:key", "./api/files.ts"),
+	route("/api/dev/purge", "./api/purge.tsx"),
+
+	// Catch-all Route
 	route("*", "./views/catch-all.tsx"),
 ];
