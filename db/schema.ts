82a23bc52c96f22e27ce6665944b5206df95773e
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-20 03:55:51 -0500

    Update schema and re-generate migrations

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { ID_LENGTH, cuid } from "./helpers/id";
 import { createdAt, timestamp, updatedAt } from "./helpers/timestamp";
 
@@ -6,33 +6,54 @@ import { createdAt, timestamp, updatedAt } from "./helpers/timestamp";
  * This table is where the application will store the data related to each user
  * of the application.
  */
-export const users = sqliteTable("users", {
-	id: cuid("id", "users_id_unique"),
-	// Timestamps
-	createdAt,
-	updatedAt,
-	emailVerifiedAt: timestamp("email_verified_at"),
-	// Attributes
-	displayName: text("display_name", { mode: "text" }),
-	email: text("email", { mode: "text" }).notNull(),
-	avatarKey: text("avatar_key", { mode: "text", length: ID_LENGTH }),
-	role: text("role", { mode: "text", enum: ["user", "root"] })
-		.notNull()
-		.default("user"),
-});
+export const users = sqliteTable(
+	"users",
+	{
+		id: cuid("id", "users_id_unique"),
+		// Timestamps
+		createdAt,
+		updatedAt,
+		emailVerifiedAt: timestamp("email_verified_at"),
+		// Attributes
+		displayName: text("display_name", { mode: "text" }),
+		email: text("email", { mode: "text" }).unique().notNull(),
+		avatarKey: text("avatar_key", { mode: "text", length: ID_LENGTH }),
+		role: text("role", { mode: "text", enum: ["user", "root"] }).default(
+			"user",
+		),
+		emailVerificationToken: cuid(
+			"email_verification_token",
+			"users_email_verification_token_unique",
+		),
+	},
+	(table) => {
+		return {
+			emailIndex: index("users_email_idx").on(table.email),
+		};
+	},
+);
 
-export const audits = sqliteTable("audit_logs", {
-	id: cuid("id", "audit_logs_id_unique"),
-	// Timestamps
-	createdAt,
-	// Attributes
-	action: text("action", { mode: "text" }),
-	auditable: text("auditable", { mode: "text" }),
-	// Relationships
-	userId: text("user_id", { mode: "text" })
-		.notNull()
-		.references(() => users.id),
-});
+export const audits = sqliteTable(
+	"audit_logs",
+	{
+		id: cuid("id", "audit_logs_id_unique"),
+		// Timestamps
+		createdAt,
+		// Attributes
+		action: text("action", { mode: "text" }),
+		auditable: text("auditable", { mode: "text" }),
+		payload: text("payload", { mode: "json" }).default(JSON.stringify({})),
+		// Relationships
+		userId: text("user_id", { mode: "text" })
+			.notNull()
+			.references(() => users.id),
+	},
+	(t) => {
+		return {
+			userIndex: index("audit_logs_user_id_idx").on(t.userId),
+		};
+	},
+);
 
 export const teams = sqliteTable("teams", {
 	id: cuid("id", "teams_id_unique"),
@@ -43,36 +64,59 @@ export const teams = sqliteTable("teams", {
 	name: text("name", { mode: "text" }),
 });
 
-export const memberships = sqliteTable("memberships", {
-	id: cuid("id", "memberships_id_unique"),
-	// Timestamps
-	createdAt,
-	updatedAt,
-	acceptedAt: timestamp("accepted_at"),
-	// Attributes
-	role: text("role", { mode: "text", enum: ["member", "admin"] })
-		.notNull()
-		.default("member"),
-	// Relationships
-	userId: text("user_id", { mode: "text" })
-		.notNull()
-		.references(() => users.id),
-	teamId: text("team_id", { mode: "text" })
-		.notNull()
-		.references(() => teams.id),
-});
+export const memberships = sqliteTable(
+	"memberships",
+	{
+		id: cuid("id", "memberships_id_unique"),
+		// Timestamps
+		createdAt,
+		updatedAt,
+		acceptedAt: timestamp("accepted_at"),
+		// Attributes
+		role: text("role", { mode: "text", enum: ["member", "admin"] })
+			.notNull()
+			.default("member"),
+		// Relationships
+		userId: text("user_id", { mode: "text" })
+			.notNull()
+			.references(() => users.id),
+		teamId: text("team_id", { mode: "text" })
+			.notNull()
+			.references(() => teams.id),
+	},
+	(t) => {
+		return {
+			userIndex: index("memberships_user_id_idx").on(t.userId),
+			userTeamIndex: index("memberships_user_team_idx").on(t.userId, t.teamId),
+			teamIndex: index("memberships_team_id_idx").on(t.teamId),
+			teamUserIndex: index("memberships_team_user_idx").on(t.teamId, t.userId),
+		};
+	},
+);
 
-export const credentials = sqliteTable("users_credentials", {
-	id: cuid("id", "users_credentials_id_unique"),
-	// Timestamps
-	createdAt,
-	updatedAt,
-	// Attributes
-	passwordHash: text("password_hash", { mode: "text" }).notNull(),
-	// Relationships
-	userId: text("user_id", { mode: "text" })
-		.notNull()
-		.references(() => users.id),
-});
+export const credentials = sqliteTable(
+	"users_credentials",
+	{
+		id: cuid("id", "users_credentials_id_unique"),
+		// Timestamps
+		createdAt,
+		updatedAt,
+		// Attributes
+		passwordHash: text("password_hash", { mode: "text" }).notNull(),
+		resetToken: text("reset_token", { mode: "text" }).unique(),
+		// Relationships
+		userId: text("user_id", { mode: "text" })
+			.notNull()
+			.references(() => users.id),
+	},
+	(t) => {
+		return {
+			userIdIndex: index("users_credentials_user_id_idx").on(t.userId),
+			resetTokenIndex: index("users_credentials_reset_token_idx").on(
+				t.resetToken,
+			),
+		};
+	},
+);
 
-export default { users, teams, memberships, credentials };
+export default { users, audits, teams, memberships, credentials };
