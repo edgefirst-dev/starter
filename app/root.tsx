48d64960cfc95774b0f2e3a762addc87970a321f
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-20 03:47:51 -0500

    Update landing page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import type * as Route from "types:+types.root";
 import type { ReactNode } from "react";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
@@ -6,14 +5,17 @@ import "./assets/tailwind.css";
 
 export function Layout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			className="bg-white text-neutral-950 dark:bg-black dark:text-neutral-50"
+		>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
 				<Links />
 			</head>
-			<body className="m-8">
+			<body className="flex flex-col min-h-dvh w-screen">
 				{children}
 				<ScrollRestoration />
 				<Scripts />
@@ -22,11 +24,6 @@ export function Layout({ children }: { children: ReactNode }) {
 	);
 }
 
-export default function App(props: Route.ComponentProps) {
-	return (
-		<>
-			<h1>Root</h1>
-			<Outlet />
-		</>
-	);
+export default function App() {
+	return <Outlet />;
 }
--- app/views/home.tsx
+++ app/views/home.tsx
@@ -1,12 +1,26 @@
-import { UsersRepository } from "app:repositories.server/users";
 import type * as Route from "types:views/+types.home";
-import { json } from "react-router";
-
-export async function loader() {
-	let users = await new UsersRepository().findAll();
-	return json({ count: users.length });
-}
 
 export default function Home(props: Route.ComponentProps) {
-	return <h2 className="text-4xl">Home</h2>;
+	return (
+		<main className="flex justify-center items-center flex-col min-h-dvh gap-8">
+			<header className="flex flex-col gap-4 text-center">
+				<h1 className="font-bold text-3xl tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+					Edge-first Starter Kit for React
+				</h1>
+				<p className="mx-auto max-w-[700px] text-neutral-500 md:text-xl dark:text-neutral-400">
+					Build lightning-fast React applications with edge computing. Deploy
+					globally and scale effortlessly.
+				</p>
+			</header>
+
+			<div className="flex gap-4">
+				<a
+					className="inline-flex items-center justify-center rounded-md bg-neutral-900 px-10 py-3 font-medium text-base text-neutral-50 shadow transition-colors hover:bg-neutral-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-neutral-50 dark:text-neutral-900 dark:focus-visible:ring-neutral-300 dark:hover:bg-neutral-50/90"
+					href="https://github.com/edgefirst-dev/starter"
+				>
+					Get Started
+				</a>
+			</div>
+		</main>
+	);
 }
