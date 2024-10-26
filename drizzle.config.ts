00b09a94b64507346929fd187d3da7e673df79a2
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-26 02:15:27 -0500

    Add comments to Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,12 +11,13 @@ if (!CLOUDFLARE_DATABASE_ID) throw new Error("Missing Cloudflare database ID");
 if (!CLOUDFLARE_API_TOKEN) throw new Error("Missing Cloudflare API token");
 
 export default defineConfig({
-	dialect: "sqlite",
-	driver: "d1-http",
-	migrations: { prefix: "timestamp" },
-	strict: true,
-	schema: "./db/schema.ts",
-	out: "./db/migrations",
+	dialect: "sqlite", // D1 uses SQLite
+	driver: "d1-http", // Configure to use D1 over HTTP
+	migrations: { prefix: "timestamp" }, // Add a timestamp prefix to migrations
+	strict: true, // Enable strict mode
+	schema: "./db/schema.ts", // Set the schema file path
+	out: "./db/migrations", // Set where we will store the migrations
+	// Configure the database credentials so it can connect to Cloudflare
 	dbCredentials: {
 		accountId: CLOUDFLARE_ACCOUNT_ID,
 		databaseId: CLOUDFLARE_DATABASE_ID,
