108519858b55490118f8369fdfa53a8f3d45fc71
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-28 12:41:42 -0500

    Add TeamsRepository#count method

--- app/repositories.server/teams.ts
+++ app/repositories.server/teams.ts
@@ -1,12 +1,12 @@
 import type { Membership } from "app:entities/membership";
 import { Team } from "app:entities/team";
-import { teams } from "db:schema";
+import schema from "db:schema";
 import { orm } from "@edgefirst-dev/core";
-import { eq } from "drizzle-orm";
+import { count, eq } from "drizzle-orm";
 
 export class TeamsRepository {
-	async create(input: Omit<typeof teams.$inferInsert, "id">) {
-		let [team] = await orm().insert(teams).values(input).returning();
+	async create(input: Omit<typeof schema.teams.$inferInsert, "id">) {
+		let [team] = await orm().insert(schema.teams).values(input).returning();
 		if (team) return Team.from(team);
 		throw new Error("Failed to create team");
 	}
@@ -14,10 +14,18 @@ export class TeamsRepository {
 	async findByMembership(membership: Membership) {
 		let items = await orm()
 			.select()
-			.from(teams)
-			.where(eq(teams.id, membership.teamId))
+			.from(schema.teams)
+			.where(eq(schema.teams.id, membership.teamId))
 			.execute();
 
 		return Team.fromMany(items);
+	}
+
+	async count() {
+		let [result] = await orm()
+			.select({ count: count() })
+			.from(schema.teams)
+			.execute();
+		return result?.count ?? 0;
 	}
 }
