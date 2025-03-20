3a835f4936e7fef5310d0f631f64b789e9729156
Sergio Xalambrí <hello@sergiodxa.com>
2025-03-20 18:38:25 -0500

    Enhance react-router configuration for server-side rendering; declare module extensions for future compatibility options

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,3 +1,20 @@
 import type { Config } from "@react-router/dev/config";
+import "react-router";
 
-export default {} satisfies Config;
+declare module "react-router" {
+	interface Future {
+		unstable_middleware: false;
+	}
+}
+
+export default {
+	// Config options...
+	// Server-side render by default, to enable SPA mode set this to `false`
+	ssr: true,
+	future: {
+		unstable_middleware: false,
+		unstable_optimizeDeps: true,
+		unstable_splitRouteModules: true,
+		unstable_viteEnvironmentApi: true,
+	},
+} satisfies Config;
